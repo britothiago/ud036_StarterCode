60851e50d2081a93d1c3a451a44b6dcdfa80a044
britothiago <britotr@gmail.com>
2018-11-01 17:53:48 -0300

    Projeto site de filmes concluido

--- media.py
+++ media.py
@@ -1,5 +1,4 @@
 class Movie():
-    """ Classe de objetos Movie. Para instanciá-lo, importe o arquivo media.py e crie o objeto chamando media.Movie() """
     def __init__(self, titulo, elenco, genero, cartaz, link_youtube):
         self.titulo = titulo
         self.elenco = elenco
