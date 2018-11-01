03ff9c64eb7090695b55f6720e50c3ab9a486012
britothiago <britotr@gmail.com>
2018-11-01 15:56:51 -0300

    Projeto site de filmes concluido

---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+    def __init__(self, titulo, elenco, genero, cartaz, link_youtube):
+        self.titulo = titulo
+        self.elenco = elenco
+        self.genero = genero
+        self.cartaz = cartaz
+        self.link_youtube = link_youtube
